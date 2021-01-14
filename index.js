57bedcb1e2f9a431d2af1a33b690b0f040c3a0f2
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2021-01-13 18:04:32 -0700

    Change Starting Size

--- index.js
+++ index.js
@@ -3,31 +3,11 @@ const createDesktopShortcut = require('create-desktop-shortcuts');
 
 const name = "transparency";
 
-function createWindow() {
-    let win = new BrowserWindow({
-        width: 600,
-        height: 400,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        icon: __dirname + '/site/favicon.ico'
-    });
-}
-
-if(process.argv.length > 1 && process.argv[1] != '.'){
-  const shortcutsCreated = createDesktopShortcut({
-      windows: {
-          filePath: `%appdata%\\..\\local\\${name}\\${name}.exe`,
-          icon: __dirname + '\\site\\favicon.ico'
-      }
-  });
-}
-
 
 function createWindow() {
     let win = new BrowserWindow({
-        width: 600,
-        height: 400,
+        width: 900,
+        height: 550,
         webPreferences: {
             nodeIntegration: true
         },
